b7f7e7bc69f46e69bc9a2da8608a8bf3ea03ff4d
avth <amsavarthini@me.com>
2017-07-24 17:31:21 -0400

    Add files via upload

--- AutomationPracticeTestNG.java
+++ AutomationPracticeTestNG.java
@@ -5,12 +5,16 @@
  * Automating the reading username, password as parameters from XML file, Logging to the web app and processing the request.
  * This program is run as TestNG Suite.
  * XML file : automationPracticeTestSuite.xml
+ * 
+ * Revision #1 : 7/24/2017
+ * Added random sleep time for thread in "processRequest" method
  */
 package com.amsa.testng;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 import org.testng.log4testng.Logger;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -26,6 +30,11 @@ public class AutomationPracticeTestNG {
 	Actions action;
 	static WebDriverWait wait;
 	SoftAssert softAssert=new SoftAssert();
+	
+	static final int maxTime=5000;	//	in milliseconds
+	static final int minTime=2000;	//	in milliseconds
+	static Random generator=new Random();
+	int timeToSleep=0;
 		
 	@BeforeTest
 	public void initializations() {
@@ -39,9 +48,6 @@ public class AutomationPracticeTestNG {
 	
 	@BeforeTest(dependsOnMethods = {"initializations"})
 	public void launchBrowser() {
-		/*System.setProperty("webdriver.gecko.driver","C:/geckodriver.exe");
-		driver=new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);*/
 		driver.get("http://www.automationpractice.com/");
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath(".//*[@id='header']/div[2]/div/div/nav/div[1]/a")).click();
@@ -70,7 +76,9 @@ public class AutomationPracticeTestNG {
 		new Select(driver.findElement(By.id("selectProductSort"))).selectByVisibleText("Price: Lowest first");
 		//	wait for the page to be refreshed and loaded.
 		try { 
-			Thread.sleep(3000);  
+			timeToSleep=generator.nextInt(maxTime-minTime) + minTime;
+			System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " :" +  timeToSleep);
+			Thread.sleep(timeToSleep);  
 		} catch(Exception e) {e.printStackTrace(); }	
 		action.moveToElement(driver.findElement(By.xpath(".//*[@id='center_column']/ul/li[1]/div/div[1]/div/a[1]/img"))).perform();
 		action.moveToElement(driver.findElement(By.xpath(".//*[@id='center_column']/ul/li[1]/div/div[3]/div[1]/a"))).click().perform();
